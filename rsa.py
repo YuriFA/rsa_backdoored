e6f6674b0ddb6531429f4fc98c6f612793bf5f66
YuriFA <yurik1452@mail.ru>
2016-06-03 23:26:09 +0300

    change

--- rsa.py
+++ rsa.py
@@ -6,18 +6,18 @@ import backdoor
 
 BIT_COUNT = 2048
 
-#файл с исходным текст
-TEXT_PATH = 'text.txt'
+#файл с исходным текстом
+TEXT_PATH = 'example/text.txt'
 #файл с зашифрованным текстом
-ENC_PATH = 'encrypt.bin'
+ENC_PATH = 'example/encrypt.bin'
 #файл с расшифрованным
-DEC_PATH = 'dec_text.txt'
+DEC_PATH = 'example/dec_text.txt'
 
 class RSABD():
 	def __init__(self, bit_count):
 		#save block size
-		self.b_sz = (bit_count + bit_count) // 8
-		self.rsa = backdoor.backdoor_rsa()
+		self.b_sz = (bit_count + bit_count) // 16
+		self.rsa = backdoor.backdoor_rsa(bit_count)
 		self.dp = self.rsa.d % (self.rsa.p - 1)
 		self.dq = self.rsa.d % (self.rsa.q - 1)
 		self.qinv = gmpy.invert(self.rsa.q, self.rsa.p)
@@ -33,11 +33,11 @@ class RSABD():
 		with open(text_path, 'r') as handle:
 			for block in iter(partial(handle.read, self.b_sz), ""):
 				text += block
-		with open(enc_path, 'wb+') as handle:
+		with open(enc_path, 'wb') as handle:
 			for i in range(0, len(text), self.b_sz - 1):
 				sliced = text[i:i + min(len(text), self.b_sz - 1)].encode()
+				print(sliced)
 				enc_block = pow(int.from_bytes(sliced, byteorder='big', signed=False), self.rsa.e, self.rsa.n)
-				print(enc_block.to_bytes(self.b_sz, byteorder='big'))
 				handle.write(enc_block.to_bytes(self.b_sz, byteorder='big'))
 
 	def decrypt(self, enc_path, dec_path):
@@ -56,7 +56,9 @@ class RSABD():
 				m2 = pow(enc_block, self.dq, self.rsa.q)
 				h = (self.qinv * (m1 - m2)) % self.rsa.p
 				m = int(m2 + h * self.rsa.q)
-				dec_block = m.to_bytes((m.bit_length() // 8) + 1, byteorder='big').decode()
+				m_b = m.to_bytes((m.bit_length() // 8) + 1, byteorder='big')
+				print(m_b)
+				dec_block = m_b.decode()
 				dec += dec_block
 		with open(dec_path, 'w') as handle:
 			handle.write(dec)
